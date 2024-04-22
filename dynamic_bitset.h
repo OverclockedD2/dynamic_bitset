799f7d2b1208530f2eb5dc06ed4661bdf326ea5b
OverclockedD2 <overclockedd2@gmail.com>
2024-04-22 17:33:41 +0300

    Add files via upload

--- dynamic_bitset.h
+++ dynamic_bitset.h
@@ -1,131 +1,344 @@
 /*
 *                       dynamic_bitset.h
-* 
+*
 *   This is a header-only, CUDA compatible dynamic bitset library.
 *   Orignial version made by github.com/OverclockedD2. Make sure to
 *   read the documentation: github.com/OverclockedD2/dynamic_bitset.
-* 
-*                   Copyroght (c) 2024 OverclockedD2
-* 
-*           Permission is hereby granted, free of charge, 
-*           to any person obtaining a copy of this software 
-*           and associated documentation files (the "Software"), 
-*           to deal in the Software without restriction, 
-*           including without limitation the rights to use, 
-*           copy, modify, merge, publish, distribute, sublicense, 
-*           and/or sell copies of the Software, and to permit 
-*           persons to whom the Software is furnished to do so, 
-*           subject to the following conditions: 
 *
-*           The above copyright notice and this permission notice 
-*           shall be included in all copies or substantial portions 
+*                   Copyright (c) 2024 OverclockedD2
+*
+*           Permission is hereby granted, free of charge,
+*           to any person obtaining a copy of this software
+*           and associated documentation files (the "Software"),
+*           to deal in the Software without restriction,
+*           including without limitation the rights to use,
+*           copy, modify, merge, publish, distribute, sublicense,
+*           and/or sell copies of the Software, and to permit
+*           persons to whom the Software is furnished to do so,
+*           subject to the following conditions:
+*
+*           The above copyright notice and this permission notice
+*           shall be included in all copies or substantial portions
 *           of the Software.
-* 
-*           THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF 
-*           ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-*           TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-*           PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-*           THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-*           DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-*           CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
-*           CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+*
+*           THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+*           ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+*           TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+*           PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+*           THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+*           DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+*           CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+*           CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 *           IN THE SOFTWARE.
 */
 
 #pragma once
 
-#include <stdexcept>
 #include <string>
+#include <exception>
 
-class dynamic_bitset {
-private:
-    int m_size = 0;
-    char* bytes = nullptr;
-public:
-    bool operator[](int index) {
-        if (index < m_size) {
-            return bytes[index / 8] >> (index % 8) & 1;
-        }
-        else {
-            throw std::out_of_range("Specified bitset index is out of range!");
-        }
-    }
-    int size() {
-        return m_size;
-    }
-    std::string get_string() {
-        std::string binary = "";
-        for (int i = 0; i < m_size; i++) {
-            binary += std::to_string(((bytes[i / 8] >> (i % 8)) & 1));
-        }
-        return binary;
-    }
-    void set(int index) {
-        if (index < m_size) {
-            bytes[index / 8] |= 1 << (index % 8);
-        }
-        else {
-            throw std::out_of_range("Specified bitset index is out of range!");
-        }
-    }
-    void unset(int index) {
-        if (index < m_size) {
-            bytes[index / 8] &= ~(1 << (index % 8));
-        }
-        else {
-            throw std::out_of_range("Specified bitset index is out of range!");
-        }
-    }
-    void toggle(int index) {
-        if (index < m_size) {
-            bytes[index / 8] ^= 1 << (index % 8);
-        }
-        else {
-            throw std::out_of_range("Specified bitset index is out of range!");
-        }
-    }
-    void pop(int index) {
-        if (index < m_size) {
-            char* new_bytes = new char[((m_size - 1) % 8 == 0) ? ((m_size - 1) / 8) : ((m_size - 1) / 8 + 1)](); // Calculate amount of bytes needed to store new size
-            for (int i = 0; i < index; i++) {
-                if (((bytes[i / 8] >> (i % 8)) & 1) == 1) new_bytes[i / 8] |= (1 << (i % 8)); // If bits[i] is 1, set new bits[i] to 1
-                else new_bytes[i / 8] &= ~(1 << (i % 8)); // If bits[i] is 0, set new bits[i] to 0
-            }
-            for (int i = index; i < m_size; i++) {
-                if (((bytes[i / 8] >> (i % 8)) & 1) == 1) new_bytes[(i - 1) / 8] |= (1 << ((i - 1) % 8)); // If bits[i] is 1, set new bits[i-1] to 1
-                else new_bytes[(i - 1) / 8] &= ~(1 << ((i - 1) % 8)); // If bits[i] is 0, set new bits[i-1] to 0
-            }
+namespace dynamic_bitset {
+	class dynamic_bitset {
+	public:
+		bool initialized = false;
+		unsigned int size = 0;
+		char* bytes = nullptr;
 
-            m_size--; // Update size
+		void push_i(unsigned int index, bool bit) {
+			if (initialized) {
+				if (index <= size) {
+					char* new_bytes = new char[(++size + 7) / 8]();
+					for (unsigned int i = 0; i < index; i++) {
+						if ((((bytes[i / 8]) >> (i % 8)) & 1) == 1) {
+							new_bytes[i / 8] |= 1 << (i % 8);
+						}
+						else {
+							new_bytes[i / 8] &= ~(1 << (i % 8));
+						}
+					}
+					if (bit == 1) {
+						new_bytes[index / 8] |= 1 << (index % 8);
+					}
+					else {
+						new_bytes[index / 8] &= ~(1 << (index % 8));
+					}
+					for (unsigned int i = index + 1; i < size; i++) {
+						if ((((bytes[(i - 1) / 8]) >> ((i - 1) % 8)) & 1) == 1) {
+							new_bytes[i / 8] |= 1 << (i % 8);
+						}
+						else {
+							new_bytes[i / 8] &= ~(1 << (i % 8));
+						}
+					}
+					delete[] bytes;
+					bytes = new_bytes;
 
-            bytes = NULL;
-            delete[] bytes;
-            bytes = new_bytes;
-            new_bytes = NULL;
-            delete[] new_bytes;
-        }
-        else {
-            throw std::out_of_range("Specified bitset index is out of range!");
-        }
-    }
-    void push_back(bool bit) {
-        char* new_bytes = new char[((m_size + 1) % 8 == 0) ? ((m_size + 1) / 8) : ((m_size + 1) / 8 + 1)](); // Calculate amount of bytes needed to store new size
-        for (int i = 0; i < m_size; i++) {
-            if (((bytes[i / 8] >> (i % 8)) & 1) == 1) new_bytes[i / 8] |= (1 << (i % 8)); // If bits[i] is 1, set new bits[i] to 1
-            else new_bytes[i / 8] &= ~(1 << (i % 8)); // If bits[i] is 0, set new bits[i] to 0
-        }
-        if (bit == 1) new_bytes[(m_size + 1) / 8] |= 1 << (m_size % 8); // If new bit is 1 set new bits[last] to 1
-        else new_bytes[(m_size + 1) / 8] &= ~(1 << (m_size % 8)); // If new bit is 0 set new bits[last] to 0
+				}
+				else {
+					throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED INDEX OUT OF RANGE!");
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void pop_i(unsigned int index) {
+			if (initialized) {
+				if (index < size) {
+					char* new_bytes = new char[(--size + 7) / 8]();
+					for (unsigned int i = 0; i < index; i++) {
+						if ((((bytes[i / 8]) >> (i % 8)) & 1) == 1) {
+							new_bytes[i / 8] |= 1 << (i % 8);
+						}
+						else {
+							new_bytes[i / 8] &= ~(1 << (i % 8));
+						}
+					}
+					for (unsigned int i = index + 1; i < size + 1; i++) {
+						if ((((bytes[i / 8]) >> (i % 8)) & 1) == 1) {
+							new_bytes[(i - 1) / 8] |= 1 << ((i - 1) % 8);
+						}
+						else {
+							new_bytes[(i - 1) / 8] &= ~(1 << ((i - 1) % 8));
+						}
+					}
+					delete[] bytes;
+					bytes = new_bytes;
+				}
+				else {
+					throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED INDEX OUT OF RANGE!");
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void set_i(unsigned int index) {
+			if (initialized) {
+				if (index < size) {
+					bytes[index / 8] |= 1 << (index % 8);
+				}
+				else {
+					throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED INDEX OUT OF RANGE!");
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void set() {
+			if (initialized) {
+				for (unsigned int i = 0; i < size; i++) {
+					bytes[i / 8] |= 1 << (i % 8);
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void unset_i(unsigned int index) {
+			if (initialized) {
+				if (index < size) {
+					bytes[index / 8] &= ~(1 << (index % 8));
+				}
+				else {
+					throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED INDEX OUT OF RANGE!");
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void unset() {
+			if (initialized) {
+				for (unsigned int i = 0; i < size; i++) {
+					bytes[i / 8] &= ~(1 << (i % 8));
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void toggle_i(unsigned int index) {
+			if (initialized) {
+				if (index < size) {
+					bytes[index / 8] ^= 1 << (index % 8);
+				}
+				else {
+					throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED INDEX OUT OF RANGE!");
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void toggle() {
+			if (initialized) {
+				for (unsigned int i = 0; i < size; i++) {
+					bytes[i / 8] ^= 1 << (i % 8);
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void swap(unsigned int index_1, unsigned int index_2) {
+			if (initialized) {
+				if (index_1 < size) {
+					if (index_2 < size) {
+						if (((bytes[index_1 / 8] >> (index_1 % 8)) & 1) == 1) {
+							if (((bytes[index_2 / 8] >> (index_2 % 8)) & 1) == 1) {
 
-        m_size++; // Update size
+							}
+							else {
+								bytes[index_1 / 8] &= ~(1 << (index_1 % 8));
+								bytes[index_2 / 8] |= 1 << (index_2 % 8);
+							}
+						}
+						else {
+							if (((bytes[index_2 / 8] >> (index_2 % 8)) & 1) == 1) {
+								bytes[index_1 / 8] |= 1 << (index_1 % 8);
+								bytes[index_2 / 8] &= ~(1 << (index_2 % 8));
+							}
+							else {
 
-        bytes = NULL;
-        delete[] bytes;
-        bytes = new_bytes;
-        new_bytes = NULL;
-        delete[] new_bytes;
-    }
-    void destroy() {
-        delete[] bytes;
-    }
-};
+							}
+						}
+					}
+					else {
+						throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED INDEX OUT OF RANGE!");
+					}
+				}
+				else {
+					throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED INDEX OUT OF RANGE!");
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void reverse() {
+			if (initialized) {
+				char* new_bytes = new char[(size + 7) / 8]();
+				for (unsigned int i = 0; i < size; i++) {
+					if (((bytes[i / 8] >> (i % 8)) & 1) == 1) {
+						new_bytes[(size - i - 1) / 8] |= 1 << ((size - i - 1) % 8);
+					}
+					else {
+						new_bytes[(size - i - 1) / 8] &= ~(1 << ((size - i - 1) % 8));
+					}
+				}
+				delete[] bytes;
+				bytes = new_bytes;
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		void extend(unsigned int amount, bool default_bit) {
+			if (initialized) {
+				char* new_bytes = new char[(size + amount + 7) / 8]();
+				for (unsigned int i = 0; i < size; i++) {
+					if (((bytes[i / 8] >> (i % 8)) & 1) == 1) {
+						new_bytes[i / 8] |= 1 << (i % 8);
+					}
+					else {
+						new_bytes[i / 8] &= ~(1 << (i % 8));
+					}
+				}
+				if (default_bit == 1) {
+					for (unsigned int i = size; i < size + amount; i++) {
+						new_bytes[i / 8] |= 1 << (i % 8);
+					}
+				}
+				else {
+					for (unsigned int i = size; i < size + amount; i++) {
+						new_bytes[i / 8] &= ~(1 << (i % 8));
+					}
+				}
+
+				size += amount;
+
+				delete[] bytes;
+				bytes = new_bytes;
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+
+		void set_string(std::string input) {
+			if (initialized) {
+				char* new_bytes = new char[(input.length() + 7) / 8]();
+				for (unsigned int i = 0; i < input.length(); i++) {
+					if (input[i] == '1') {
+						new_bytes[i / 8] |= 1 << (i % 8);
+					}
+					else if (input[i] == '0') {
+						new_bytes[i / 8] &= ~(1 << (i % 8));
+					}
+					else {
+						throw std::exception("[ZEROES & ONES] ERROR: INVALID CHARACTER IN INPUT STRING!");
+					}
+				}
+				size = input.length();
+
+				delete[] bytes;
+				bytes = new_bytes;
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+		}
+		std::string get_string() {
+			if (initialized) {
+				std::string output = "";
+				for (unsigned int i = 0; i < size; i++) {
+					if (((bytes[i / 8] >> (i % 8)) & 1) == 1) {
+						output += '1';
+					}
+					else {
+						output += '0';
+					}
+				}
+				return output;
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED BITSET HAS NOT BEEN INITIALIZED!");
+			}
+			return "";
+		}
+		bool get_bit(unsigned int index) {
+			if (initialized) {
+				if (index < size) {
+					return (bytes[index / 8] >> (index % 8)) & 1;
+				}
+			}
+			else {
+				throw std::exception("[ZEROES & ONES] ERROR: SPECIFIED INDEX OUT OF RANGE!");
+			}
+			return 0;
+		}
+
+
+		dynamic_bitset() {
+			if (not initialized) {
+				initialized = true;
+				size = 0;
+				bytes = new char[0];
+			}
+		}
+		~dynamic_bitset() {
+			if (initialized) {
+				initialized = false;
+				delete[] bytes;
+			}
+		}
+		void destroy() {
+			if (initialized) {
+				initialized = false;
+				delete[] bytes;
+			}
+		}
+	};
+}
